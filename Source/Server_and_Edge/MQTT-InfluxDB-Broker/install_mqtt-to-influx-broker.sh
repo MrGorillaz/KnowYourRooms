a05cc1abc6fdd8302739dd8f60f952947ac1019c
MrGorillaz <Alexander_Orlowski@gmx.de>
2024-07-27 12:03:07 +0200

    Update install_mqtt-to-influx-broker.sh

--- Source/Server_and_Edge/MQTT-InfluxDB-Broker/install_mqtt-to-influx-broker.sh
+++ Source/Server_and_Edge/MQTT-InfluxDB-Broker/install_mqtt-to-influx-broker.sh
@@ -4,9 +4,13 @@ echo "Run this script with SUDO!!!!"
 
 #Getting needed Resources
 echo -n "Installing needed Resources..."
-wget -qO- https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
-export DISTRIB_ID=$(lsb_release -si); export DISTRIB_CODENAME=$(lsb_release -sc)
-echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list > /dev/null
+#wget -qO- https://repos.influxdata.com/influxdb.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/influxdb.gpg > /dev/null
+#export DISTRIB_ID=$(lsb_release -si); export DISTRIB_CODENAME=$(lsb_release -sc)
+#echo "deb [signed-by=/etc/apt/trusted.gpg.d/influxdb.gpg] https://repos.influxdata.com/${DISTRIB_ID,,} ${DISTRIB_CODENAME} stable" | sudo tee /etc/apt/sources.list.d/influxdb.list > /dev/null
+curl -sL https://repos.influxdata.com/influxdb.key | apt-key add -
+source /etc/os-release
+test $VERSION_ID = "7" && echo "deb https://repos.influxdata.com/debian wheezy stable" | tee -a /etc/apt/sources.list
+test $VERSION_ID = "8" && echo "deb https://repos.influxdata.com/debian jessie stable" | tee -a /etc/apt/sources.list
 sudo apt-get update && sudo apt-get install influxdb2 -y
 sudo apt-get install mosquitto -y
 sudo apt-get install python3-pip -y
